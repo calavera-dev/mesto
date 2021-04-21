feee7975c868e44e3a2179dbfcdd6496d48e1ff7
Alexandr Levchenko <calavera-skull@yandex.ru>
2021-04-22 00:48:34 +0300

    первая иттерация

--- scripts/index.js
+++ scripts/index.js
@@ -11,28 +11,21 @@ popupOpenButton.addEventListener('click', togglePopup);
 popupCloseButton.addEventListener('click', togglePopup);
 
 
+
 let formElement = document.querySelector('.popup__container');
 
 function formSubmitHandler (evt) {
 	evt.preventDefault();
-
-// Находим поля формы в DOM
 	let nameInput = formElement.querySelector('.popup__name');
 	let jobInput = formElement.querySelector('.popup__description');
-
-// Получите значение полей из свойства value
-  let nameValue = nameInput.getAttribute('value');
-  let jobValue = jobInput.getAttribute('value');
-
-	// Выберите элементы, куда должны быть вставлены значения полей
+  let nameValue = nameInput.value;
+  let jobValue = jobInput.value;
   let profileName = document.querySelector('.profile__name');
   let profileOcupation = document.querySelector('.profile__description');
-
-	// Вставьте новые значения с помощью textContent
+  let savePopup = document.querySelector('.popup__submit');
   profileName.textContent = nameValue;
   profileOcupation.textContent = jobValue;
+  savePopup.addEventListener('click', togglePopup);
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
