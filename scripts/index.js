24294e24dad99f53e1bba291cb3a49b211a1f727
Alexandr Levchenko <calavera-skull@yandex.ru>
2021-04-24 18:23:41 +0300

    убран лишний импорт, поменяны теги кнопки, убран лишний параметр элемента popup__input, добавлено присвоение значений инпутам при открытии попапа

--- scripts/index.js
+++ scripts/index.js
@@ -7,19 +7,20 @@ let jobInput = formElement.querySelector('.popup__input_user_occupation');
 let profileName = document.querySelector('.profile__name');
 let profileOcupation = document.querySelector('.profile__description');
 
-function openPopup (evt) {
-  evt.preventDefault();
+function openPopup() {
   popup.classList.add('popup_is-opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileOcupation.textContent;
 }
-function closePopup (evt) {
-  evt.preventDefault();
+
+function closePopup() {
   popup.classList.remove('popup_is-opened');
 }
-function formSubmitHandler (evt) {
-	evt.preventDefault();
+function formSubmitHandler(evt) {
+  evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileOcupation.textContent = jobInput.value;
-  closePopup(evt);
+  closePopup();
 }
 
 popupOpenButton.addEventListener('click', openPopup);
