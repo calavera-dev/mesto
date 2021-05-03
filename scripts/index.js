c89077e0f08defc33fe181af8540322fce68774d
Alexandr Levchenko <calavera-skull@yandex.ru>
2021-05-03 20:35:34 +0300

    динамически добавлен контент на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,43 @@ let jobInput = formElement.querySelector('.popup__input_user_occupation');
 let profileName = document.querySelector('.profile__name');
 let profileOcupation = document.querySelector('.profile__description');
 
+const placeGrid = document.querySelector('.places');
+const placeTemplate = document.querySelector('#place-template').content;
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function(currentCard) {
+  const placesItem = placeTemplate.querySelector('.places__item').cloneNode(true);
+  placesItem.querySelector('.places__image').src = currentCard.link;
+  placesItem.querySelector('.places__title').textContent = currentCard.name;
+  placeGrid.append(placesItem);
+});
+
 function openPopup() {
   popup.classList.add('popup_is-opened');
   nameInput.value = profileName.textContent;
